608b6da04508d2667014450c3e59dd05f7ca7091
natrapNiko <natrapniko@gmail.com>
2025-07-02 17:00:49 +0200

    Add Category class to data models
    Introduced a new `Category` class in the `ChopChop.Data.Models` namespace. The class contains three properties: `Id` (int), `Name` (non-nullable string), and `Recipes` (collection of `Recipe` objects initialized as a `HashSet<Recipe>`).
    
    Translate:
    Добавяне на клас Категория към моделите на данни
    
    Въведен е нов клас `Category` в пространството от имена `ChopChop.Data.Models`. Класът съдържа три свойства: (int), `Name` (string, който не може да се нулира) и `Recipes` (колекция от обекти `Recipe`, инициализирана като `HashSet<Recipe>`).

---
+++ ChopChop/Data/Models/Category.cs
@@ -0,0 +1,11 @@
+﻿namespace ChopChop.Data.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+
+        public virtual ICollection<Recipe> Recipes { get; set; } = new HashSet<Recipe>();
+
+    }
+}
