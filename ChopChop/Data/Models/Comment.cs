34bebe46e127e1e02dd7b41237592edaa1764b3f
natrapNiko <natrapniko@gmail.com>
2025-07-02 17:06:58 +0200

    Add Comment class to data models
    Introduced a new `Comment` class in the `ChopChop.Data.Models` namespace.
    This class features properties for `Id`, `Content`, `CreatedDate`,
    `UserId`, and `RecipeId`, along with navigation properties for
    `User` and `Recipe`. Required properties are initialized to
    `null!` to ensure they are not null at runtime.
    
    Translate:
    Добавяне на клас Comment към моделите на данни
    
    Въведен е нов клас `Comment` в пространството от имена `ChopChop.Data.Models`.
    Този клас съдържа свойства за `Id`, `Content`, `CreatedDate`,
    `UserId` и `RecipeId`, както и навигационни свойства за
    `User` и `Recipe`. Задължителните свойства се инициализират на
    `null!`, за да се гарантира, че те не са null по време на изпълнение.

---
+++ ChopChop/Data/Models/Comment.cs
@@ -0,0 +1,19 @@
+﻿
+namespace ChopChop.Data.Models
+{
+    using Microsoft.AspNetCore.Identity;
+
+    public class Comment
+    {
+        public int Id { get; set; }
+        public string Content { get; set; } = null!;
+        public DateTime CreatedDate { get; set; }
+
+        public string UserId { get; set; } = null!;
+        public virtual IdentityUser User { get; set; } = null!;
+
+        public int RecipeId { get; set; }
+        public virtual Recipe Recipe { get; set; } = null!;
+
+    }
+}
