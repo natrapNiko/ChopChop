6de88610b2f6a41fdaa01f68b2ed3017030b4326
natrapNiko <natrapniko@gmail.com>
2025-07-02 17:21:23 +0200

    Add user-related models for application functionality
    This commit introduces three new classes: `ApplicationUser`, `Favorite`, and `UserProfile` within the `ChopChop.Data.Models` namespace. The `ApplicationUser` class extends `IdentityUser` and includes properties for user profiles, recipes, comments, and favorites. The `Favorite` class links users to their favorite recipes, while the `UserProfile` class stores user-specific information such as display name and profile image URL. Necessary using directives for `Microsoft.AspNetCore.Identity` and `System.ComponentModel.DataAnnotations` have also been added.
    
    Translate:
    Добавяне на модели, свързани с потребителя, за функционалността на приложението
    
    С този commit се въвеждат три нови класа: `ApplicationUser`, `Favorite` и `UserProfile` в рамките на пространството от имена `ChopChop.Data.Models`. Класът `ApplicationUser` разширява `IdentityUser` и включва свойства за потребителски профили, рецепти, коментари и любими. Класът `Favorite` свързва потребителите с техните любими рецепти, докато класът `UserProfile` съхранява специфична за потребителя информация, като например име на дисплея и URL адрес на профилното изображение. Добавени са и необходимите директиви за използване на `Microsoft.AspNetCore.Identity` и `System.ComponentModel.DataAnnotations`.

---
+++ ChopChop/Data/Models/ApplicationUser.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace ChopChop.Data.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public virtual UserProfile Profile { get; set; } = null!;
+        public virtual ICollection<Recipe> Recipes { get; set; } = new HashSet<Recipe>();
+        public virtual ICollection<Comment> Comments { get; set; } = new HashSet<Comment>();
+        public virtual ICollection<Favorite> Favorites { get; set; } = new HashSet<Favorite>();
+
+    }
+}
---
+++ ChopChop/Data/Models/UserProfile.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace ChopChop.Data.Models
+{
+    public class UserProfile
+    {
+        [Key]
+        public string UserId { get; set; } = null!;
+        public string DisplayName { get; set; } = null!;
+        public string? ProfileImageUrl { get; set; }
+        public virtual IdentityUser User { get; set; } = null!;
+    }
+}
