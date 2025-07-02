b89817caed5ac3a6250ed49a38420f7461373e2b
natrapNiko <natrapniko@gmail.com>
2025-07-02 16:58:53 +0200

    Add Ingredient class to data models
    Introduced a new `Ingredient` class in the `ChopChop.Data.Models` namespace. The class includes properties for `Id`, `Name`, `Quantity`, `RecipeId`, and a navigation property `Recipe`. String properties are initialized to disallow null values.
    
    Translate:
    Добавяне на клас Ingredient към моделите на данни
    
    Въведен е нов клас `Ingredient` в пространството от имена `ChopChop.Data.Models`. Класът включва свойства за `Id`, `Name`, `Quantity`, `RecipeId` и навигационно свойство `Recipe`. Свойствата на низовете са инициализирани така, че да не допускат нулеви стойности.

---
+++ ChopChop/Data/Models/Ingredient.cs
@@ -0,0 +1,11 @@
+﻿namespace ChopChop.Data.Models
+{
+    public class Ingredient 
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Quantity { get; set; } = null!;
+        public int RecipeId { get; set; }
+        public virtual Recipe Recipe { get; set; } = null!;
+    }
+}
